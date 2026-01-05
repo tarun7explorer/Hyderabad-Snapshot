f71ae4b790f9adb355a88beb85f9079a0a3fa561
tarun7explorer <tarungajibimkar@gmail.com>
2026-01-05 14:38:59 +0530

    Removed branding

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
-      <Sonner />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
